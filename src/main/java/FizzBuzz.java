67085120ca245aac8de1cad3f6cd6c379926f408
merleandrade <andrade.merle@gmail.com>
2022-07-01 11:26:22 +0200

    CI Testen

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,12 @@
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+
+    }
+    public static String transform(int num){
+        String result = num+"";
+        result = (num% 2 == 0 || num% 3 == 0)?"Fizz": result;
+        result = (num% 3 == 0)?"Buzz": result;
+        return result;
+    }
+}
